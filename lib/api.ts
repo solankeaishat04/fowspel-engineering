f94593139f0bcec0d4d97f3bc934128dc5b5602c
Solanke Aishat <aishatsolanke123@gmail.com>
2026-02-04 10:48:44 +0100

    fix: api bug fix

--- lib/api.ts
+++ lib/api.ts
@@ -1,14 +1,14 @@
-// lib/api.ts
-// import { supabase } from "./supabase";
-import { ContactInput } from "@/schemas/useContact";
+// // lib/api.ts
+// // import { supabase } from "./supabase";
+// import { ContactInput } from "@/schemas/useContact";
 
-export async function submitContact(data: ContactInput) {
-  const { error } = await supabase.from("contacts").insert([data]);
+// export async function submitContact(data: ContactInput) {
+//   const { error } = await supabase.from("contacts").insert([data]);
 
-  if (error) {
-    console.error("Supabase Insert Error:", error);
-    throw new Error(error.message);
-  }
+//   if (error) {
+//     console.error("Supabase Insert Error:", error);
+//     throw new Error(error.message);
+//   }
 
-  return true;
-}
+//   return true;
+// }
